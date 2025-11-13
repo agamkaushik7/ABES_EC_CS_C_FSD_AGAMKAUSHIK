0cc3b2eadfc9952f96bd3b85e8274f4b2e05ab6a
agamkaushik7 <agamkaushik2@gmail.com>
2025-11-13 13:06:09 +0530

    day_13

---
+++ day_11/my/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+
+const Header=()=>{
+  return(
+  <>
+ Header
+  </>);
+}
+const Body=()=>{
+  return(<>
+  Body
+  </>)
+}
+const Footer=()=>{
+  return(
+    <>
+    Footer
+    </>
+  )
+}
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Body/>
+      <Footer/>
+
+    </div>
+  )
+}
+
+export default App  
